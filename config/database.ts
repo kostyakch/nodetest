0516fc151eb35095610c3e07aa1b74aab64216aa
Konstantin Chernyaev <kch@list.ru>
2020-10-29 22:44:52 +0300

    Add mocha & dotenv

--- config/database.ts
+++ config/database.ts
@@ -1,2 +1,2 @@
 import { Sequelize } from "sequelize";
-export const database = new Sequelize("postgres://postgres:postgres@0.0.0.0:5432/postgres");
+export const database = new Sequelize(process.env.DATABASE_URL);
--- lib/server.ts
+++ lib/server.ts
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 import app from "./app";
 const PORT = process.env.PORT || 3000;
 
